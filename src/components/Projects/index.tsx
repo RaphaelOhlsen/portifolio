db52fd04046b76f7371790c396543a191cfdc186
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2023-06-12 18:37:42 -0300

    delete section error

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -7,17 +7,17 @@ export default function Projects({ data }) {
   return (
     <Container>
       <SectionTitle title=" Ultimos Projetos" />
-      <section>
-        {data.map(project => (
-          <ProjectItem
-            key={project.id}
-            title={project.title}
-            type={project.type}
-            slug={project.uid}
-            img={project.image}
-          />
-        ))}
-      </section>
+
+      {data.map(project => (
+        <ProjectItem
+          key={project.id}
+          title={project.title}
+          type={project.type}
+          slug={project.uid}
+          img={project.image}
+        />
+      ))}
+
       <button type="button">
         <NextLink href="/projects">Ver todos os projetos</NextLink>
       </button>
